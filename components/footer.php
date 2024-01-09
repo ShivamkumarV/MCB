e9371031b0702b1c4320d5ac652d374128bc8374
ShivamkumarV <99964891+ShivamkumarV@users.noreply.github.com>
2024-01-09 11:46:37 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+
+  
+
+   <div class="credit"> DESIGNED BY <span>RAVINESH SINGH and SHIVAM KUMAR V</span> | BCA 'A' SEC!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
